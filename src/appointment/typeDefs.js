fe8d2d6eac0beb4ae5fbe287460ba6c40f48ca5d
alekzu <rafaguaa@unal.edu.co>
2019-11-16 19:07:46 -0500

    appo id int to string

--- src/appointment/typeDefs.js
+++ src/appointment/typeDefs.js
@@ -18,11 +18,11 @@ input aScheduleInput {
 
 export const appointmentQueries = `
     allschedules: [schedule]!
-    scheduleById(id: Int!): schedule!
+    scheduleById(id: String!): schedule!
 `;
 
 export const appointmentMutations = `
     createSchedule(sch: aScheduleInput!): schedule!
-    updateSchedule(id: Int!, sch: aScheduleInput!): schedule!
-    deleteSchedule(id: Int!): Int!
+    updateSchedule(id: String!, sch: aScheduleInput!): schedule!
+    deleteSchedule(id: String!): Int!
 `;
